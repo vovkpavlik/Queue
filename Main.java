5eccd5b5a83c708649a26cdc99fd95c88e7c9856
vovkpavlik <vovk.pavlik@bk.ru>
2023-03-01 15:43:08 +0300

    some changes

--- Main.java
+++ Main.java
@@ -7,7 +7,14 @@ public class Main {
         Queue<String> q = new Queue<>();
         q.addObj("Ahah");
         q.addObj("Homorjoba");
+        q.showQueue();
         q.whoIsAfrontOfYou("Homorjoba");
+        System.out.println(q.getSize());
+        q.remove();
+        q.whoIsAfrontOfYou("Ahah");
+        q.showQueue();
+        System.out.println(q.getSize());
+        q.showFirst();
 
     }
 }
@@ -15,44 +22,52 @@ public class Main {
 class Queue<T> {
     ArrayList<T> queue = new ArrayList<>();
 
-    //Чекаем, пуста ли очередь.
-    public void checkIsEmpty(ArrayList<T> queue) {
-        if (queue.isEmpty()) {
-            System.out.println("Очередь пуста");
-        }
-    }
-
     public void addObj(T obj) {
         queue.add(obj);
     }
 
     public void showQueue() {
-        checkIsEmpty(queue);
-        for (T man: queue) {
-            System.out.println(man);
+        if (queue.isEmpty()) {
+            System.out.println("Очередь пуста");
+        } else {
+            for (T man : queue) {
+                System.out.println(man);
+            }
         }
     }
 
     public void showFirst() {
-        checkIsEmpty(queue);
-        System.out.println(queue.get(0));
+        if (queue.isEmpty()) {
+            System.out.println("Очередь пуста");
+        } else {
+            System.out.println(queue.get(0));
+        }
     }
 
     public void remove() {
-        checkIsEmpty(queue);
-        queue.remove(0);
+        if (queue.isEmpty()) {
+            System.out.println("Очередь пуста");
+        } else {
+            queue.remove(0);
+        }
     }
 
     public int getSize() {
-        checkIsEmpty(queue);
-        return queue.size();
+            if (!queue.isEmpty()) {
+                return queue.size();
+            } else {
+                System.out.println("Очередь пуста");
+            }
+        return 0;
     }
 
     //Извините, а вы за кем?
     public void whoIsAfrontOfYou(String man) {
-        checkIsEmpty(queue);
         int index = queue.indexOf(man);
-        if (index == 0) {
+        if (queue.isEmpty()) {
+            System.out.println("Очередь пуста");
+        }
+        else if (index == 0) {
             System.out.println("Ни закем. Я следующий.");
         } else if (!queue.contains(man)) {
             System.out.println("Меня не так зовут!");
